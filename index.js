b2158e40c59ca3d3a95ef7b794e766012dd13514
Agustin Barrientos <39675693+agustinbarrientos@users.noreply.github.com>
2022-01-07 21:07:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,4 @@ const tiny = ( variable ) => {
   return modifiedString;
 }
 
-module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  return string.replace(/\s/g, "");
-};
+module.exports = tiny;
